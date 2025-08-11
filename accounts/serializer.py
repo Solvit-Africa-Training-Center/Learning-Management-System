583135fa8cd80991f922a5703ed66c2910b07c09
Ismail gihozo <gihozoismail@gmial.com>
2025-08-11 10:51:42 +0200

    user creattion

---
+++ accounts/serializer.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+
+from accounts.models import CustomUser
+
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    first_name=serializers.CharField(required=True)
+    username=serializers.CharField(required=True)
+    last_name=serializers.CharField(required=True)
+    phone=serializers.CharField(required=True)
+    email=serializers.EmailField(required=True)
+    password=serializers.CharField(required=True, write_only=True)
+    re_type_password=serializers.CharField(required=True, write_only=True)
+    class Meta:
+        model=CustomUser
+        fields=["first_name","username","last_name","email","phone","password","re_type_password","role"]
+        extra_kwargs={
+            "password": {"write_only": True},
+            "re_type_password": {"write_only": True},
+            "role": {"required": False}
+        }
+
+    def validate(self,data):
+        if   data["password"]!=data["re_type_password"]   :
+            raise serializers.ValidationError("Passwords do not match")
+        return data
+    def create(self, validated_data):
+        user=CustomUser.objects.create_user(
+            username=validated_data["username"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+            email=validated_data["email"],
+            phone=validated_data["phone"],
+            password=validated_data["password"],
+            role=validated_data.get("role", "Guest")
+        )
+        return user
---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+from  django.urls import path,include
+from .views import RegisterUserViewSet
+from rest_framework import routers
+
+router=routers.DefaultRouter()
+router.register("user",RegisterUserViewSet)
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("",include(router.urls))
+    
+]
